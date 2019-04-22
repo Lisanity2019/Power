7e920246f46556f342c9355a50ac0bccb3f64e39
liyao <liyao@192.168.0.100>
2019-04-23 07:33:40 +0800

    重构爬虫dgc

--- DGCspider/DGCspider/spiders/dgc.py
+++ DGCspider/DGCspider/spiders/dgc.py
@@ -6,27 +6,21 @@ from DGCspider.items import DgcspiderItem
 class DgcSpider(scrapy.Spider):
     name = 'dgc'
     # allowed_domains = ['www.meitulu.com']
-    my_url = "https://www.meitulu.com/t/yanni/"
+    my_url = "https://www.meitulu.com/t/1273/"
     start_urls = [my_url]
-    temp_url_list = [my_url]
-    temp_url_second_list = []
-    i = 0
-    j = 0
+
+  
 
     def parse(self, response):
         # 根据姓名分类获取所有图集url地址
         base_url = response.xpath("//ul[@class='img']/li/a/@href").extract()
         for img_index_url in base_url:
-            self.temp_url_second_list.append(img_index_url)
-
             yield scrapy.Request(img_index_url, callback=self.parse_index)
-        # 如果有多页，就提取其他页url地址
 
+        # 当前图集如果有多页，就提取其他页url地址，发送请求
         base_nextpage_url = response.xpath(
             "//div[@id='pages']/a[4]/@href").extract()[0]
-        if (base_nextpage_url != self.temp_url_list[self.i]):
-            self.i += 1
-            self.temp_url_list.append(base_nextpage_url)
+        if (base_nextpage_url):
             scrapy.Request(base_nextpage_url, callback=self.parse)
 
     def parse_index(self, response):
@@ -49,11 +43,7 @@ class DgcSpider(scrapy.Spider):
         # 循环爬取下一页
         next_page_tmp = response.xpath(
             "//div[@id='pages']/a[last()]/@href").extract()[0]
-        baseurl = "https://www.meitulu.com"
-        next_page = baseurl + next_page_tmp
-
-        if (next_page != self.temp_url_second_list[self.j]):
-            self.i += 1
-            self.temp_url_second_list.append(next_page)
-            print("^^^^^^^^^^^^^^^^^^^^^^^^"+response.urljoin(next_page_tmp)+"^^^^^^^^^^^^^^^^^^^^^^^^")
-            yield scrapy.Request(next_page, callback=self.parse_index)
+        if(next_page_tmp):
+            # 拼接绝对路径
+            next_page_url = response.urljoin(next_page_tmp)
+            yield scrapy.Request(next_page_url, callback=self.parse_index)  # 默认已去重        
